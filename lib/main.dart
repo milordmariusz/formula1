70d3aa04e1c412fd02d8badc775e372e3eddf773
milordmariusz <komodo454@wp.pl>
2022-11-07 10:08:21 +0100

    add basic view to application

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,191 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Math Keyboard Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DemoPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DemoPage extends StatefulWidget {
+  const DemoPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DemoPageState createState() => _DemoPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DemoPageState extends State<DemoPage> {
+  @override
+  Widget build(BuildContext context) {
+    const upperTab = SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(top: 10),
+        child: TabBar(tabs: <Tab>[
+          Tab(icon: Icon(Icons.filter_9_plus_outlined)),
+          Tab(icon: Icon(Icons.book)),
+          Tab(icon: Icon(Icons.settings)),
+        ]),
+      ),
+    );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: upperTab,
+        ),
+        body: TabBarView(
+          children: [
+            const _MathFieldTextFieldExample(),
+            _MathEquationsDataBase(),
+            const _ClearableAutofocusExample(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MathFieldTextFieldExample extends StatelessWidget {
+  const _MathFieldTextFieldExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Strona do edycji równań"),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            height: 100,
+            margin: const EdgeInsets.all(30),
+            child: const TextField(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Icon(size: 100,color: Colors.black, Icons.delete_forever),
+                ),
+              ),
+              SizedBox(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Icon(size: 100,color: Colors.black, Icons.add),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _MathEquationsDataBase extends StatelessWidget {
+  _MathEquationsDataBase({
+    Key? key,
+  }) : super(key: key);
+
+  final equationsList = [
+    "Równanie: 1",
+    "Równanie: 2",
+    "Równanie: 3",
+    "Równanie: 4",
+    "Równanie: 5",
+    "Równanie: 6",
+    "Równanie: 7",
+    "Równanie: 8",
+    "Równanie: 9",
+    "Równanie: 10",
+    "Równanie: 11",
+    "Równanie: 12",
+    "Równanie: 13",
+    "Równanie: 14",
+    "Równanie: 15",
+    "Równanie: 16",
+    "Równanie: 17",
+    "Równanie: 18",
+    "Równanie: 19",
+    "Równanie: 20",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: equationsList.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                borderRadius: const BorderRadius.all(Radius.circular(20))),
+            width: double.infinity,
+            height: 150,
+            child: Align(
+              alignment: const Alignment(-0.9, 0),
+              child: Text(
+                equationsList[index],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ClearableAutofocusExample extends StatefulWidget {
+  const _ClearableAutofocusExample({Key? key}) : super(key: key);
+
+  @override
+  _ClearableAutofocusExampleState createState() =>
+      _ClearableAutofocusExampleState();
+}
+
+class _ClearableAutofocusExampleState
+    extends State<_ClearableAutofocusExample> {
+  late final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return const SafeArea(
+      child: Center(
+        child: Text(
+          "Tutaj będą ustawienia aplikacji",
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
