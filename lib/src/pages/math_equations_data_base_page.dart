4af8b54ea24ab031a03c17543b37410e72d6b157
milordmariusz <komodo454@wp.pl>
2022-12-23 17:23:28 +0100

    Add rendering equations

--- lib/src/pages/math_equations_data_base_page.dart
+++ lib/src/pages/math_equations_data_base_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tex/flutter_tex.dart';
 import 'package:formula1/src/custom_widgets/navigation_drawer/navigation_drawer.dart';
 import 'package:formula1/src/shared/math_formulas.dart';
 import 'package:formula1/src/strings/strings.dart';
@@ -26,7 +27,6 @@ class _MathEquationsDataBasePageState extends State<MathEquationsDataBasePage> {
 
   @override
   Widget build(BuildContext context) {
-    print(mathFormulasList);
     return Scaffold(
       appBar: AppBar(title: const Text(Strings.mathEquationDataBasePageTitle)),
       drawer: const NavigationDrawer(
@@ -40,23 +40,25 @@ class _MathEquationsDataBasePageState extends State<MathEquationsDataBasePage> {
             padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
             child: Container(
               decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        color: ColorPalette.blackMaterialColor.shade400,
-                        blurRadius: 25.0,
-                        offset: const Offset(0, 10))
-                  ],
-                  color: ColorPalette.lightGreyMaterialColor.shade200,
-                  borderRadius: const BorderRadius.all(Radius.circular(20))),
+                boxShadow: [
+                  BoxShadow(
+                    color: ColorPalette.blackMaterialColor.shade400,
+                    blurRadius: 25.0,
+                    offset: const Offset(0, 10),
+                  )
+                ],
+                color: ColorPalette.lightGreyMaterialColor.shade200,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
               width: double.infinity,
               height: 150,
               child: Align(
                 alignment: const Alignment(-0.9, 0),
-                child: Text(
-                  mathFormulasList[index],
-                  style: TextStyle(
-                    color: ColorPalette.black,
-                    fontSize: 30,
+                child: TeXView(
+                  child: TeXViewDocument(
+                    "\$\$${mathFormulasList[index]}\$\$",
                   ),
                 ),
               ),
