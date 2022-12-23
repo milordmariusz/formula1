a52d98a54715eeee5948a9cf50aa16224315f2ae
milordmariusz <komodo454@wp.pl>
2022-12-23 15:59:47 +0100

    Shared Preferences works now

--- lib/src/pages/math_equations_data_base_page.dart
+++ lib/src/pages/math_equations_data_base_page.dart
@@ -4,58 +4,71 @@ import 'package:formula1/src/shared/math_formulas.dart';
 import 'package:formula1/src/strings/strings.dart';
 import 'package:formula1/src/style/color_palette.dart';
 
-class MathEquationsDataBasePage extends StatelessWidget {
+class MathEquationsDataBasePage extends StatefulWidget {
   const MathEquationsDataBasePage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MathEquationsDataBasePage> createState() =>
+      _MathEquationsDataBasePageState();
+}
+
+class _MathEquationsDataBasePageState extends State<MathEquationsDataBasePage> {
+  var shared = MathFormulas();
+  List<String> mathFormulasList = [];
+
+  @override
+  void initState() {
+    getMathFormulasFromStorage();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(mathFormulasList);
     return Scaffold(
       appBar: AppBar(title: const Text(Strings.mathEquationDataBasePageTitle)),
       drawer: const NavigationDrawer(
         selectedPage: 1,
       ),
-      body: ValueListenableBuilder(
-        valueListenable: MathFormulas(),
-        builder: (mathFormula, value, child) {
-          final mathFormulas = value;
-          return ListView.builder(
-            padding: const EdgeInsets.only(top: 35),
-            itemCount: mathFormulas.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            color: ColorPalette.blackMaterialColor.shade400,
-                            blurRadius: 25.0,
-                            offset: const Offset(0, 10))
-                      ],
-                      color: ColorPalette.lightGreyMaterialColor.shade200,
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(20))),
-                  width: double.infinity,
-                  height: 150,
-                  child: Align(
-                    alignment: const Alignment(-0.9, 0),
-                    child: Text(
-                      mathFormulas[index],
-                      style: TextStyle(
-                        color: ColorPalette.black,
-                        fontSize: 30,
-                      ),
-                    ),
+      body: ListView.builder(
+        padding: const EdgeInsets.only(top: 35),
+        itemCount: mathFormulasList.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
+            child: Container(
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: ColorPalette.blackMaterialColor.shade400,
+                        blurRadius: 25.0,
+                        offset: const Offset(0, 10))
+                  ],
+                  color: ColorPalette.lightGreyMaterialColor.shade200,
+                  borderRadius: const BorderRadius.all(Radius.circular(20))),
+              width: double.infinity,
+              height: 150,
+              child: Align(
+                alignment: const Alignment(-0.9, 0),
+                child: Text(
+                  mathFormulasList[index],
+                  style: TextStyle(
+                    color: ColorPalette.black,
+                    fontSize: 30,
                   ),
                 ),
-              );
-            },
+              ),
+            ),
           );
         },
-        // child:
       ),
     );
+  }
+
+  Future<void> getMathFormulasFromStorage() async {
+    mathFormulasList = await shared.getMathFormulas();
+    setState(() {});
   }
 }
