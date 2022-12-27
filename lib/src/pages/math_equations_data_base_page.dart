e32ad3238f54750cb76305e8c77e73209a8484d2
milordmariusz <komodo454@wp.pl>
2022-12-27 10:19:08 +0100

    Add remove items

--- lib/src/pages/math_equations_data_base_page.dart
+++ lib/src/pages/math_equations_data_base_page.dart
@@ -36,29 +36,36 @@ class _MathEquationsDataBasePageState extends State<MathEquationsDataBasePage> {
         padding: const EdgeInsets.only(top: 35),
         itemCount: mathFormulasList.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
-            child: Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: ColorPalette.blackMaterialColor.shade400,
-                    blurRadius: 25.0,
-                    offset: const Offset(0, 10),
-                  )
-                ],
-                color: ColorPalette.lightGreyMaterialColor.shade200,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(20),
+          return Dismissible(
+            key: Key(mathFormulasList[index]),
+            onDismissed: (direction) {
+              deleteMathFormula(index);
+            },
+            background: Container(color: Colors.red),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: ColorPalette.blackMaterialColor.shade400,
+                      blurRadius: 25.0,
+                      offset: const Offset(0, 10),
+                    )
+                  ],
+                  color: ColorPalette.lightGreyMaterialColor.shade200,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
                 ),
-              ),
-              width: double.infinity,
-              height: 150,
-              child: Align(
-                alignment: const Alignment(-0.9, 0),
-                child: TeXView(
-                  child: TeXViewDocument(
-                    "\$\$${mathFormulasList[index]}\$\$",
+                width: double.infinity,
+                height: 150,
+                child: Align(
+                  alignment: const Alignment(-0.9, 0),
+                  child: TeXView(
+                    child: TeXViewDocument(
+                      "\$\$${mathFormulasList[index]}\$\$",
+                    ),
                   ),
                 ),
               ),
@@ -72,5 +79,15 @@ class _MathEquationsDataBasePageState extends State<MathEquationsDataBasePage> {
   Future<void> getMathFormulasFromStorage() async {
     mathFormulasList = await shared.getMathFormulas();
     setState(() {});
+  }
+
+  void deleteMathFormula(int index) {
+    mathFormulasList.removeAt(index);
+    shared.saveMathFormulas(mathFormulasList);
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Wzór usunięty'),
+      ),
+    );
   }
 }
